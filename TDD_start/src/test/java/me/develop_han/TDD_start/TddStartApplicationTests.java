1036e6ec160a85012e153a8aa4adf24cc11812e9
SeungHan <jk05018@naver.com>
2022-03-21 12:03:26 +0900

    tdd-start start

---
+++ TDD_start/src/test/java/me/develop_han/TDD_start/TddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.develop_han.TDD_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
