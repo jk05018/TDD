45f345819a13ef9870e84d587eedd6a24ca23ad8
SeungHan <jk05018@naver.com>
2022-03-22 15:43:19 +0900

    refactor : test 코드 중 겹치는 부분 refactoring

--- TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrengthMeterTest.java
+++ TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrengthMeterTest.java
@@ -6,36 +6,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
+	PasswordStrengthMeter meter = new PasswordStrengthMeter();
 
 	@DisplayName("1. 모든 조건을 충족한다면 암호 강도는 강함이어야 함")
 	@Test
 	public void meetsAllCriteria_Then_Strong() throws Exception {
-
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab12!@AB");
-		assertEquals(result, PasswordStrength.STRONG);
-
-		PasswordStrength result2 = meter.meter("abc1!Add");
-		assertEquals(result2, PasswordStrength.STRONG);
+		assertStrength("ab12!@AB", PasswordStrength.STRONG);
+		assertStrength("abc1!Add", PasswordStrength.STRONG);
 
 	}
 
 	@DisplayName("2. 길이만 8글자 미만이고 나머지 조건은 충족하는 경우")
 	@Test
 	public void meetOtherCriteria_except_for_Length_Then_Normal() throws Exception {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab12!@A");
-		assertEquals(result, PasswordStrength.NORMAL);
-
-		PasswordStrength result2 = meter.meter("AB12!c");
-		assertEquals(result2, PasswordStrength.NORMAL);
+		assertStrength("ab12!@A", PasswordStrength.NORMAL);
+		assertStrength("AB12!c", PasswordStrength.NORMAL);
 	}
 
 	@DisplayName("3. 숫자르 포함하지 않고 나머지 조건을 충족한 경우")
 	@Test
 	public void meetsOtherCreteria_except_for_number_Then_Normal() throws Exception {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab!@ABqwer");
-		assertEquals(result, PasswordStrength.NORMAL);
+		assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+	}
+
+	private void assertStrength(String s, PasswordStrength expStr) {
+		PasswordStrength result = meter.meter(s);
+		assertEquals(result, expStr);
 	}
 }
