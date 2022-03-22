00e6479bf8563b7fb68032920706ec26090620b6
SeungHan <jk05018@naver.com>
2022-03-22 15:39:33 +0900

    feat : 암호해독기 test 구현
    test1 : 모든 조건을 충족하는 경우
    test2 : 길이만 8글자 미만이고 나머지 조건은 충족하는 경우
    test3 : 숫자를 포함하지 않고 나머지 조건은 충족하는 경우

--- TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrength.java
+++ TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrength.java
@@ -1,2 +1,5 @@
-package me.develop_han.TDD_start;public enum PasswordStrength {
+package me.develop_han.TDD_start;
+
+public enum PasswordStrength {
+	STRONG,NORMAL,WEEK
 }
--- TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrengthMeterTest.java
+++ TDD_start/src/test/java/me/develop_han/TDD_start/PasswordStrengthMeterTest.java
@@ -1,15 +1,41 @@
 package me.develop_han.TDD_start;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DecoderTest {
+public class PasswordStrengthMeterTest {
 
-	@DisplayName("1. 테스트")
+	@DisplayName("1. 모든 조건을 충족한다면 암호 강도는 강함이어야 함")
 	@Test
-	public void test() throws Exception{
-		int value = 0;
+	public void meetsAllCriteria_Then_Strong() throws Exception {
 
-		Decoder.check()
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab12!@AB");
+		assertEquals(result, PasswordStrength.STRONG);
+
+		PasswordStrength result2 = meter.meter("abc1!Add");
+		assertEquals(result2, PasswordStrength.STRONG);
+
+	}
+
+	@DisplayName("2. 길이만 8글자 미만이고 나머지 조건은 충족하는 경우")
+	@Test
+	public void meetOtherCriteria_except_for_Length_Then_Normal() throws Exception {
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab12!@A");
+		assertEquals(result, PasswordStrength.NORMAL);
+
+		PasswordStrength result2 = meter.meter("AB12!c");
+		assertEquals(result2, PasswordStrength.NORMAL);
+	}
+
+	@DisplayName("3. 숫자르 포함하지 않고 나머지 조건을 충족한 경우")
+	@Test
+	public void meetsOtherCreteria_except_for_number_Then_Normal() throws Exception {
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab!@ABqwer");
+		assertEquals(result, PasswordStrength.NORMAL);
 	}
 }
