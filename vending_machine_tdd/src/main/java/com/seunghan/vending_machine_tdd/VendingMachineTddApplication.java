8a2d659fe188b28123c951788f5520af9cba9bdd
SeungHan <jk05018@naver.com>
2022-04-03 11:26:43 +0900

    build : 초기 Project build

---
+++ vending_machine_tdd/src/main/java/com/seunghan/vending_machine_tdd/VendingMachineTddApplication.java
@@ -0,0 +1,13 @@
+package com.seunghan.vending_machine_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineTddApplication.class, args);
+	}
+
+}
---
+++ vending_machine_tdd/src/test/java/com/seunghan/vending_machine_tdd/VendingMachineTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seunghan.vending_machine_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
